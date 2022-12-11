7c8a91f0a03c28341aa0a627623b6ceec395646b
adrgs <albastroiudragos@gmail.com>
2022-12-11 15:01:29 +0100

    Add solution for Day5 Part 1

---
+++ Day5-1/solve.py
@@ -0,0 +1,41 @@
+import re
+
+def parse_stacks(lines):
+    r = re.compile(r'\[([A-Z])\]')
+    stacks = lines[-1].split()
+    stacks = [[] for _ in range(len(stacks))]
+    for line in lines[:-1]:
+        if line:
+            for match in r.finditer(line):
+                stacks[match.start()//4].append(match.group(1))
+    for stack in stacks:
+        stack.reverse()
+    return stacks
+
+def move(n, src, dest):
+    for _ in range(n):
+        dest.append(src.pop())
+
+def solve(lines):
+    ans = []
+    stacks, instructions = lines[:lines.index('\n')], lines[lines.index('\n')+1:]
+    stacks = parse_stacks(stacks)
+    
+    for instruction in instructions:
+        instruction = instruction.strip()
+        if instruction:
+            instruction = instruction.split()
+            n, src, dest = int(instruction[1]), stacks[int(instruction[3])-1], stacks[int(instruction[5])-1]
+            move(n, src, dest)
+
+    for stack in stacks:
+        ans.append(stack[-1])
+
+    print(''.join(ans))
+
+def main():
+    with open('input.txt') as f:
+        solve(f.readlines())
+
+if __name__ == '__main__':
+    main()
